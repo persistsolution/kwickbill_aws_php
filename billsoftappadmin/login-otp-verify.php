600227e51cd9c370f959dbfc8522082357ffe3c6
persistsolution <persistsolution1@gmail.com>
2025-08-21 17:11:52 +0530

    update

--- billsoftappadmin/login-otp-verify.php
+++ billsoftappadmin/login-otp-verify.php
@@ -61,7 +61,7 @@ include_once 'config.php';
                             <div class="typing">
                                 <h3>OTP Verification</h3>
                             </div>
-                            <?php if($_REQUEST['phone'] == '9595454957' || $_REQUEST['phone'] == '8956627166'){
+                            <?php if($_REQUEST['phone'] == '9595454957' || $_REQUEST['phone'] == '8767246610'){
                                 $fillotp = $_SESSION['otp'];
                                 //$fillotp = "";
                             }
